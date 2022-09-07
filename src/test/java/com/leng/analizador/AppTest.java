74aceb83239113080a62e180fa1cbf07ff995ecb
Oliver Sierra <sierra.oe@gmail.com>
2022-09-06 18:15:12 -0600

    Initiating the project

---
+++ src/test/java/com/leng/analizador/AppTest.java
@@ -0,0 +1,20 @@
+package com.leng.analizador;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
